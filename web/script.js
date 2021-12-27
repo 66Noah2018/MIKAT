07f90082e80aa2a89188b87768b945010e576dc2
renskelotte48 <49675916+renskelotte48@users.noreply.github.com>
2021-12-27 20:00:13 +0100

    auto-generate testcases table based on tests + defaults (given, not deduced) fixed sticky buttons in ribbon

--- web/script.js
+++ web/script.js
@@ -4,10 +4,52 @@
  * and open the template in the editor.
  */
 
-var parser = new DOMParser();
+let parser = new DOMParser();
+let recentlyOpened = {};
+const testcaseWindowPrologue = "<div data-role='window' data-title='Testcases' data-resizeable='false' class='p-2' id='testcases' data-on-min-click='minimizeWindow()' data-on-max-click='maximizeWindow()' data-draggable='false'>";
+const testcaseWindowEpilogue = "</div>";
 
 function getRandom(max){
     return (Math.floor(Math.random() * max));
+}
+
+function cartesian(args) {
+    var r = [], max = args.length-1;
+    function helper(arr, i) {
+        for (var j=0, l=args[i].length; j<l; j++) {
+            var a = arr.slice(0); // clone arr
+            a.push(args[i][j]);
+            if (i===max){
+                r.push(a);
+            }
+            else {
+                helper(a, i+1);
+            }
+        }
+    }
+    helper([], 0);
+    return r;
+}
+
+function generateTestcasesTableCode(variables, possibleValues){
+    // possibleValues: array of arrays with every possible value for a variable (as in neg/pos or a value within a specific range)
+    let code = "<table class='table cell-hover table-border row-border cell-border compact' id='testcases'><thead><tr><th>#</th>";
+    variables.forEach(element => {
+        code += "<th>" + element + "</th>";
+    });
+    code += "</tr></thead><tbody contenteditable>";
+    const valueCombinations = cartesian(possibleValues);
+    let caseNr = 1;
+    valueCombinations.forEach(combination => {
+        code += "<tr><td>" + caseNr + "</td>";
+        combination.forEach(value => {
+            code += "<td>" + value + "</td>";
+        });
+        code += "</tr>";
+        caseNr += 1;
+    });
+    code += "</tbody></table>";
+    return code;    
 }
 /**
  * 
@@ -15,7 +57,7 @@ function getRandom(max){
  * @returns {null}
  */
 function openProject(){
-    
+    // via Java!!
 }
 
 /**
@@ -25,7 +67,10 @@ function openProject(){
  */
 function openRecentProject(node){
     // getAttribute('data-filename') always returns the filename of the selected project. project can be opened by searching for full path in array. full path will not be included in listview
-    alert(node.getAttribute('data-filename'))
+    alert(node.getAttribute('data-filename'));
+    filename = node.getAttribute('data-filename');
+    fullPath = recentlyOpened[filename];
+    // some connection to Java code to a) check existence of file (update list if file doesn't exist) and b) load file
 }
 
 /**
@@ -65,11 +110,30 @@ function preferences(){}
 
 /**
  * 
+ * @param {type} target
  * @returns {undefined}
  */
-function generateTestcases(){
-    var spinner = $("#generate_testcases_load")[0];
-    spinner.style.display = "block";
+function generateTestcases(clicked_id){ //get variables and default values
+//    let btn = document.getElementById(clicked_id);
+//    $("#" + clicked_id).removeAttr("class");
+////    $("#" + clicked_id).attr("class", "ribbon-button");
+////    console.log(btn.getAttribute("class"));
+////    btn.classList.remove("active");
+////    btn.classList.remove("js-active");
+//    console.log(btn.classList)
+    table = document.getElementById("testcases");
+    if (table === null){
+        var spinner = $("#generate_testcases_load")[0];
+        spinner.style.display = "block";
+        let target = document.getElementsByClassName("index-body")[0];
+        const tableCode = generateTestcasesTableCode(["CRP", "ANA"], [[5, 500], ["Pos", "Neg"]]);
+        const windowCode = testcaseWindowPrologue + tableCode + testcaseWindowEpilogue;
+        target.appendChild(parser.parseFromString(windowCode, 'text/html').body.firstChild);
+        spinner.style.display = "none";
+        document.getElementById("testcases").addEventListener("input", function() {
+            console.log("input event fired");
+        }, false);
+    }
 }
 
 /**
@@ -79,8 +143,8 @@ function generateTestcases(){
  */
 function minimizeWindow(){
     var window = document.getElementById(event.path[3].id);
-    window.classList.add("minimized")
-    if (window.parentElement.classList[0] == "cell-2"){ 
+    window.classList.add("minimized");
+    if (window.parentElement.classList[0] === "cell-2"){ 
         return;
     }
     var doc = parser.parseFromString(`<div class='cell-2' id=cell${getRandom(10)}></div>`, 'text/html');
@@ -99,7 +163,7 @@ function minimizeWindow(){
 function maximizeWindow(){
     var window = document.getElementById(event.path[3].id);
     var cellid = window.parentElement.getAttribute('id');
-    if (cellid == null){
+    if (cellid === null){
         return;
     }
     var target = document.getElementsByClassName("index-body")[0];
